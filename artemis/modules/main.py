a88ee90f6a9a20c144ecff829609896bbfd7cd6d
Lio Exp <liocosta07@gmail.com>
2026-04-03 00:57:37 +0100

    feat: ATGC - parser verb/noun com validação

--- artemis/modules/main.py
+++ artemis/modules/main.py
@@ -1,3 +1,16 @@
+verbs = {
+    'v02':'mostrar dados',
+    'v03':'iniciar contagem regressiva',
+    'v04':'abortar missão',
+    'v05':'iniciar decolagem'
+    }
+    
+nouns = {
+    'n01':'posição',
+    'n02':'velocidade'
+    }
+
+
 while True:
     usuario = input("digite seu pedido: ").strip()
     if usuario == 'exit':
@@ -12,5 +25,19 @@ while True:
         partes = comando.split() 
         verb = partes[0]
         noun = partes[1]
+
+        if verb in verbs:
+            print(verbs[verb])
+        else:
+            print(f'parece que o {verb} não existe!')
+
+        if noun in nouns:
+            print(nouns[noun])
+        else:
+            print(f'parece que o {noun} não existe!')
+
     except IndexError:
         print('voce n colocou o comando completo, verifique o comando digitado')
+
+
+
